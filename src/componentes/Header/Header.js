0c20a136792cfb9e4897f57b56c50f148e7dab87
Leyo68 <leyodegario68@gmail.com>
2024-06-23 18:29:03 -0600

    org

---
+++ src/componentes/Header/Header.js
@@ -0,0 +1,10 @@
+import "./Header.css";
+
+//Ya es Componente de React
+function Header() {
+    return <header className="header">
+        <img src="/img/header.png" alt='Org' />
+    </header>
+}
+
+export default Header;
